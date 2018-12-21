692ebf1674a61c7423a90d95a3824d573f550b72
zankoav <zankoav@mail.ru>
2018-12-21 15:29:30 +0300

    create to-header html

--- header.php
+++ header.php
@@ -6,4 +6,20 @@
     <link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
 </head>
-<body <?php body_class();?>>
+<body <?php body_class(); ?>>
+<div class="top-header">
+    <div class="container">
+        <div class="top-header__inner">
+            <div class="top-header__contacts"><a class="top-header__info top-header__info_phone"
+                                                 href="tel:1-555-644-5566"><i class="fas fa-mobile-alt"></i>1-555-644-5566</a><span
+                        class="top-header__info top-header__info_location"><i class="fas fa-map-marker-alt"></i>Studio City, CA 91604</span>
+            </div>
+            <div class="top-header__search">
+                <form class="search" action="/" method="get">
+                    <input class="search__input" type="text" placeholder="Search...">
+                    <button class="search__button" type="submit"><i class="fas fa-search"></i></button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
