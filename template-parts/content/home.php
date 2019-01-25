9555bbf8c2bbe754824ac9145c7b8e6e4ca10567
zankoav <zankoav@mail.ru>
2019-01-25 15:47:07 +0300

    add pattern

--- template-parts/content/home.php
+++ template-parts/content/home.php
@@ -1,6 +1,8 @@
-<div class="container">
-	<?php
-        get_template_part( 'template-parts/content/recent-news' );
-        get_template_part( 'template-parts/content/partners' );
-	?>
-</div>
+<section class="single-wrapper">
+    <div class="container">
+		<?php
+			get_template_part( 'template-parts/content/recent-news' );
+			get_template_part( 'template-parts/content/partners' );
+		?>
+    </div>
+</section>
